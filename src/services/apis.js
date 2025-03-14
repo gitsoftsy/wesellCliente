dbb131b81e6d1f42fb6b92e103037b3fc88daa86
viniciussoftsy <168453902+viniciussoftsy@users.noreply.github.com>
2025-03-14 09:20:04 -0300

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,8 +1,8 @@
 // const url_base = 'http://ec2-18-235-243-90.compute-1.amazonaws.com:8080/api-wesell';
 const url_img = "https://api.we-sell.store";
-const url_base = "https://api.we-sell.store/api-wesell";
-const apiFinanceiro = "https://api.we-sell.store/api-venda-wesell";
+const url_base = "http://10.40.110.2:8080/api-wesell";
+const apiFinanceiro = "http://10.40.110.2:8080/api-venda-wesell";
 const apiFrete =
-  "https://api.we-sell.store/api-venda-wesell/melhor-envio/calcular";
+  "http://10.40.110.2:8080/api-venda-wesell/melhor-envio/calcular";
 
 export { url_base,apiFinanceiro, url_img, apiFrete };
